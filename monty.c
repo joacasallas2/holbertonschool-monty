5b3e19e61b90b90f854cb4415c54e6556cd3da6a
joacasallas2 <proyecta.ds@gmail.com>
2024-02-03 16:05:40 -0500

    initial general files

---
+++ monty.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * main - entry point to the monty program
+ * @argc: The number of args
+ * @argv: The pointer to the array of args
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	
+	return (0);
+}
---
+++ monty.h
@@ -0,0 +1,48 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	int (*f)(stack_t **stack, int n, unsigned int line_number);
+} instruction_t;
+
+int push(stack_t **stack, int n, unsigned int line_number);
+int pall(stack_t **stack, int n, unsigned int line_number);
+char *read_textfile(const char *filename, size_t letters);
+int (*get_op_func(char *operation))(stack_t **stack, int n, unsigned int line_number);
+char **tokenizer(char *line_ptr, char *delim);
+void free_array(char **array);
+
+#endif
